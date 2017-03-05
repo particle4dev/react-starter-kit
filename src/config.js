a0fbc3e4d06b5f98b74de5ee043b3f0ae5f732bf
hoang <particle4dev@gmail.com>
2017-03-06 01:49:17 +0700

    add FB app

--- src/config.js
+++ src/config.js
@@ -29,8 +29,8 @@ export const auth = {
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
-    secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    id: process.env.FACEBOOK_APP_ID || '669818893191009',
+    secret: process.env.FACEBOOK_APP_SECRET || 'a06b0239c7df66ab9f13e878b0d77029',
   },
 
   // https://cloud.google.com/console/project
